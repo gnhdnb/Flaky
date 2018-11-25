62c8a7d03a6b78b319175230073ec5f3be2115dc
Nikita Prudnikov <gnhdnb@gmail.com>
2018-11-25 15:34:21 +0300

    - sequential note collection tests

---
+++ Flaky.Tests/Sources/Notes/SequentialNoteCollectionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Flaky.Tests
+{
+	[TestClass]
+	public class SequentialNoteCollectionTests
+	{
+		[TestMethod]
+		public void Sequencing()
+		{
+			var collection = new SequentialNoteCollection("0-5dd2u", "parent1");
+			var context = new StubContext();
+
+			collection.Initialize(context);
+
+			collection.Update(context);
+			Assert.AreEqual(0, collection.GetNextNote().Number);
+			collection.Update(context);
+			Assert.AreEqual(null, collection.GetNextNote());
+			collection.Update(context);
+			Assert.AreEqual(-19, collection.GetNextNote().Number);
+			collection.Update(context);
+			Assert.AreEqual(14, collection.GetNextNote().Number);
+			collection.Update(context);
+			Assert.AreEqual(0, collection.GetNextNote().Number);
+		}
+	}
+}
