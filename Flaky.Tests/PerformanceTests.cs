89592c16f693a2d2eab82f96bd67e67ed33bac63
Nikita Prudnikov <gnhdnb@gmail.com>
2018-12-09 16:53:28 +0300

    performance test, update to .net 4.7.2

---
+++ Flaky.Tests/PerformanceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaky.Tests
+{
+	[TestClass]
+	public class PerformanceTests
+	{
+		[TestMethod]
+		public void StandardWorkloadTest()
+		{
+			var compiler = new Compiler(typeof(Source).Assembly);
+			var result = compiler.Compile(
+			@"
+				return 35 % Osc(1);
+			"
+			);
+
+			var source = result.Player.CreateSource();
+
+			var contextController = new ContextController(44100, 120, new Configuration());
+
+			source.Initialize(new Context(contextController));
+
+
+			var timer = new Stopwatch();
+
+			timer.Start();
+			for (long i = 0; i < 44100; i++)
+			{
+				contextController.NextSample();
+
+				source.Play(new Context(contextController));
+			}
+			timer.Stop();
+
+			Assert.Inconclusive($"{timer.ElapsedMilliseconds} ms");
+		}
+	}
+}
