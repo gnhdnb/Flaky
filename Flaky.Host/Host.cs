703a7b8eac9bcd70253e80a68549cc306b380e8f
Nikita Prudnikov <gnhdnb@gmail.com>
2019-01-06 08:44:49 +0300

    bpm, sample rate, buffer size has been exposed in public interface

--- Flaky.Host/Host.cs
+++ Flaky.Host/Host.cs
@@ -29,7 +29,7 @@ namespace Flaky
 			});
 
 			Device = new WaveOut();
-			Mixer = new Mixer(channelsCount, configuration);
+			Mixer = new Mixer(channelsCount, 13230, 44100, 120, configuration);
 			Adapter = new WaveAdapter(Mixer);
 
 			if (outputWaveFilePath != null)
