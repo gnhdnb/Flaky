d209c60fac2dd5d4b005a4d61fdd79147d597f6d
Nikita Prudnikov <gnhdnb@gmail.com>
2019-01-08 14:33:16 +0300

    one pole filter optimizations

--- Flaky.Sources/Sources/Effects/Filter/OnePoleHPFilter.cs
+++ Flaky.Sources/Sources/Effects/Filter/OnePoleHPFilter.cs
@@ -4,8 +4,9 @@ namespace Flaky
 {
 	public class OnePoleHPFilter : OnePoleFilter
 	{
-		internal OnePoleHPFilter(Source source, Source cutoff, string id) : base(source, cutoff, id) { }
-		internal OnePoleHPFilter(Source cutoff, string id) : base(cutoff, id) { }
+		internal OnePoleHPFilter(Source source, Source cutoff, string id) 
+			: base(source, cutoff, true, id) { }
+		internal OnePoleHPFilter(Source cutoff, string id) : base(cutoff, true, id) { }
 
 		protected override Vector2 GetResult(Vector2 lp, Vector2 hp)
 		{
--- Flaky.Sources/Sources/Effects/Filter/OnePoleLPFilter.cs
+++ Flaky.Sources/Sources/Effects/Filter/OnePoleLPFilter.cs
@@ -4,9 +4,11 @@ namespace Flaky
 {
 	public class OnePoleLPFilter : OnePoleFilter
 	{
-		internal OnePoleLPFilter(Source source, Source cutoff, string id) : base(source, cutoff, id) { }
+		internal OnePoleLPFilter(Source source, Source cutoff, string id) 
+			: base(source, cutoff, true, id) { }
 
-		internal OnePoleLPFilter(Source cutoff, string id) : base(cutoff, id) { }
+		internal OnePoleLPFilter(Source cutoff, string id) 
+			: base(cutoff, true, id) { }
 
 		protected override Vector2 GetResult(Vector2 lp, Vector2 hp)
 		{
