0f80752ac4e0e3c953d56d7da955b60c5d047e01
Nikita Prudnikov <gnhdnb@gmail.com>
2019-07-01 20:33:38 +0300

    opentkaudiodevice

---
+++ Flaky.Adapters/OpenTK/OpenTKAudioDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flaky
+{
+	public class OpenTKAudioDevice : IAudioDevice
+	{
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Init(IBufferedSource source, string recordFilePath = null)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Play()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Stop()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
